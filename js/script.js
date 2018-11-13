6099e80db545f87fa2d1275cc02ca7915f14bab0
Shershnev Sergey <shv.sergey70@gmail.com>
2018-11-13 12:34:02 +0300

    final

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    //Toggle menu
+    $('#menu').on('click', function() {
+        var $menu = $(this);
+        var $navigation = $('#navigation');
+        if($menu.hasClass('active')) {
+            $navigation.addClass('slideOutRight');
+            $navigation.removeClass('slideInRight');
+            $menu.removeClass('active');
+        }  else {
+            $navigation.addClass('slideInRight');
+            $navigation.removeClass('d-none slideOutRight');
+            $menu.addClass('active');
+        }
+    });
+    //Bootstrap scrollspy
+    $('body').scrollspy({
+        target: '#navigation',
+        offset: 80
+    });
+    //Scroll animation
+    $('.js-scrollspy-item').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).position().top-50
+        }, 1000);
+    });
+});
